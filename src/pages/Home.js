c1a42c13a8ab1d731dbe13d97f5e26e7c85d7b6b
Subhan Mahmood <sm01800@surrey.ac.uk>
2021-12-27 22:28:37 +0000

    added modal and logic when timer is running

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,7 +26,7 @@ export default function Home() {
 
     ]
     const [currentSession, setCurrentSession] = useState(0)
-
+    const [intermediateSesssion, setIntermediateSesssion] = useState(0)
     const [count, setCount] = useState(sessions[0].seconds)
     const [intervalId, setIntervalId] = useState(0)
 
@@ -56,7 +56,6 @@ export default function Home() {
     useEffect(() => {
         if (count === 0) {
             audio.play()
-            console.log("finished")
             clearInterval(intervalId)
             setCurrentSession((currentSession + 1) % sessions.length)
             toggleIsOn()
@@ -71,7 +70,16 @@ export default function Home() {
         setCount(sessions[currentSession].seconds)
     }, [currentSession])
 
-    
+    const setSession = (i) => {
+        setIntermediateSesssion(i)
+        if (count !== sessions[currentSession].seconds) {
+            setIsOpen(true)
+        } else {
+            setCurrentSession(i)
+        }
+    }
+
+
 
     return (
         <div className={`h-screen w-full md:p-5`} style={{ backgroundColor: dark ? '#303437' : '#fff' }}>
@@ -83,7 +91,7 @@ export default function Home() {
                                 return (
                                     <button
                                         className={`rounded px-3 py-2 text-white text-xs font-medium ${currentSession === i && "bg-neutral-400"}`}
-                                        onClick={() => { setCurrentSession(i) }}
+                                        onClick={() => { setSession(i) }}
                                     >
                                         {session.label}
                                     </button>
@@ -101,32 +109,26 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <button onClick={() => setIsOpen(true)} className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="default-modal">
-                Toggle modal
-            </button>
             {isOpen &&
-                <div id="default-modal" aria-hidden="true" className="overflow-y-auto overflow-x-hidden fixed w-full left-0 top-0 z-50 flex justify-center items-start h-modal md:h-screen md:inset-0 bg-slate-700 bg-opacity-50">
+                <div id="default-modal" aria-hidden="true" className="overflow-y-auto overflow-x-hidden fixed w-full left-0 top-0 z-50 flex justify-center items-start h-modal md:h-screen md:inset-0 bg-neutral-700 bg-opacity-50">
                     <div className="relative px-4 w-full max-w-2xl h-full md:h-auto mt-12">
-                        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                            <div className="flex justify-between items-start p-5 rounded-t border-b dark:border-gray-600">
-                                <h3 className="text-xl font-semibold text-gray-900 lg:text-2xl dark:text-white">
+                        <div className="relative bg-white rounded-lg shadow dark:bg-neutral-700">
+                            <div className="flex justify-between items-start p-5 rounded-t border-b dark:border-neutral-600">
+                                <h3 className="text-xl font-semibold text-neutral-900 lg:text-2xl dark:text-white">
                                     Terms of Service
                                 </h3>
-                                <button onClick={() => setIsOpen(false)} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="default-modal">
+                                <button onClick={() => setIsOpen(false)} type="button" className="text-neutral-400 bg-transparent hover:bg-neutral-200 hover:text-neutral-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-neutral-600 dark:hover:text-white" data-modal-toggle="default-modal">
                                     <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                                 </button>
                             </div>
-                            <div className="p-6 space-y-6">
-                                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                                    With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                                </p>
-                                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                                </p>
+                            <div className="p-6 space-y-6 dark:text-neutral-200">
+                                You have a timer running, are you sure you want to switch?
                             </div>
-                            <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
-                                <button data-modal-toggle="default-modal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                                <button data-modal-toggle="default-modal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">Decline</button>
+                            <div className="flex items-center p-6 space-x-2 rounded-b border-t border-neutral-200 dark:border-neutral-600">
+                                <button onClick={() => {
+                                    setCurrentSession(intermediateSesssion)
+                                    setIsOpen(false)}} type="button" className="text-neutral-800 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-neutral-200 dark:hover:bg-neutral-300">Yes</button>
+                                <button onClick={() => setIsOpen(false)} type="button" className="text-neutral-500 bg-white hover:bg-neutral-100 focus:ring-4 focus:ring-neutral-300 rounded-lg border border-neutral-200 text-sm font-medium px-5 py-2.5 hover:text-neutral-900 focus:z-10 dark:bg-neutral-700 dark:text-neutral-300 dark:border-neutral-500 dark:hover:text-white dark:hover:bg-neutral-600">No</button>
                             </div>
                         </div>
                     </div>
